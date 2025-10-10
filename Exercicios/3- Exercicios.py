1b963f3ffbe54cc3f31cc7bb0eb6e5313777084d
Jean Alves <jean_prog@hotmail.com>
2025-10-10 08:00:30 -0300

    Exercicios

---
+++ Exercicios/3- Exercicios.py
@@ -0,0 +1,27 @@
+# Vamos revisar funções adicionando type hints e Pydantic
+from typing import List
+
+#1- Calcular Média de Valores em uma Lista
+# def calcular_media(valores: List[float]) -> float:
+#     return sum(valores) / len(valores)
+
+#2-  Filtrar Dados Acima de um Limite
+# def filtrar_dados(valores: List[float], valor_filtro: float) -> List[float]:
+#     lista_numeros = []
+#     for valor in valores:
+#         if valor > valor_filtro:
+#             lista_numeros.append(valor)
+#     return lista_numeros
+
+# teste = filtrar_dados([8.5, 9.0, 7.5, 10.0], 9)
+# print(teste)
+
+# https://github.com/lvgalvao/data-engineering-roadmap/blob/main/Bootcamp%20-%20Python%20para%20dados/aula07/README.md
+
+#3- Contar Valores Únicos em uma Lista
+
+#4- Converter Celsius para Fahrenheit em uma Lista
+
+#5- Calcular Desvio Padrão de uma Lista
+
+#6- Encontrar Valores Ausentes em uma Sequência
